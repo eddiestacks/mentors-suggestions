9a2293dc61eb6baf70192a9baeb33b0d8dbce6f9
Cherub Kumar <cherub.kumar@gmail.com>
2014-08-13 00:08:46 -0500

    Update test query to test limit of yql queries -- Limit of 620 characters for yql when sending it through the proxy.. More if we can find a way to send directly

--- app.js
+++ app.js
@@ -9,7 +9,7 @@
         return {
           url: 'http://query.yahooapis.com/v1/public/yql',
           type: 'GET',
-          proxy_v2: true,
+          proxy_v2: false,
           data: {
             q: query,
             format: 'json'
@@ -21,14 +21,16 @@
     events: {
       'app.activated': 'init',
       'getYahooKeywords.done': 'handleYahooKeywords',
-      'getYahooKeywords.fail': 'handleYahooKeywords'
+      'getYahooKeywords.fail': 'handleFail'
 
     },
 
     init: function() {
       console.log("App has started!!")
       console.log("Making yahoo ajax request")
-      var testQuery = "Italian sculptors and painters of the renaissance favored the Virgin Mary for inspiration.";
+      // MAX Characters for Yahoo Query: 620
+      // TODO: implement hard coded limit of 620 characters for yahoo queries
+      var testQuery = "Hi, my company uses your dashboard for a more than 2 years and now we are searching for new options, like the support on Twitter is. I've watched the demo, but still have some questions. The main topic is how we can convert a tweet in to a ticket, because we found only an option for converting a DM to a tweet? Another question is can we do integrated support on the single Twitter channel for more than one product and can we measure the effort of our support team separately for different projects? Also, how can we measure it? Do we have to convert every tweet to a ticket and than measure the success based on Good ";//Another question is can we do integrated support on the single Twitter channel for more than one product and can we measure the effort of our support team separately for different projects? Also, how can we measure it? Do we have to convert every tweet to a ticket and than measure the success based on Good and Bad marks given by customer, just like the standard measuring process works? We are very serious in our intentions to implement this in our everyday support process, so can you please answer these questions or prepare some tutorial for us? Many thanks! Best regards.";
       this.fetchYahooKeywords(testQuery);
     },
 
@@ -46,15 +48,20 @@
         console.log(yql_results);
       } 
     },
+    handleFail: function(rsp) {
+      console.log(rsp)
+    },
 
     createYQLQuery: function(query) {
+      
       var yql_query_base = "select * from contentanalysis.analyze where text=";
-      var query = yql_query_base + "'" + query + "'";
+      // TODO: Join the query on '\n'
+      // todo: Test if escaping quotes is necessary...
+      // todo: append to yql_query_base
+      var query = yql_query_base + "\"" + query + "\"";
       console.log(query)
       return query;
-      // TODO: Join the query on '\n'
-      // todo: then quote it
-      // todo: append to yql_query_base
+      
     },
     // This utility function creates the query string
     // to be appended to the base URI of the YQL Web
