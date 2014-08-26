30dc1b61a604f76ebbaa4336de6d2b973f5e8331
Prateek <pchhetri@zendesk.com>
2014-08-26 16:56:32 +0800

    API Requests
    Two test calls

--- app.js
+++ app.js
@@ -1,14 +1,40 @@
 (function() {
 
-  return {
-    defaultState:'scaffolding',
-    
-    events: {
-      'app.activated':'doSomething'
-    },
+	return {
+		events: {
+			'app.activated': 'doSomething',
+			'click.searchAPI': 'searchAPITikcets',
+			'click.searchEmail': 'searchEmailTikcets'
+		},
 
-    doSomething: function() {
-    }
-  };
+		requests: {
+			getAPITikcets: function() {
+				return {
+					url: '/api/v2/search.json?query=tags:about_api',
+					type: 'GET'
+				};
+			},
+			getEmailTickets: function() {
+				return {
+					url: '/api/v2/search.json?query=tags:about_email',
+					type: 'GET'
+				};
+			}
+		},
+
+		doSomething: function() {
+			console.log("Hello world!");
+		},
+
+		searchAPITikcets: function() {
+			this.ajax('getAPITikcets');
+			console.log("I made the following request: /api/v2/search.json?query=tags:about_api");
+		},
+
+		searchEmailTikcets: function() {
+			this.ajax('getEmailTickets');
+			console.log("I made the following request: /api/v2/search.json?query=tags:about_email");
+		}
+	};
 
 }());
