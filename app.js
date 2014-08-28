8a7f640fc50a9da9ba4c1b222787b78f26be3619
Joe Kramer <jkramer@zendesk.com>
2014-08-28 14:15:45 -0500

    refactored getKeywords()

--- app.js
+++ app.js
@@ -37,105 +37,100 @@
     },
 
     getKeywords: function() {
-      var wordsArray = [];
-      var keyArray = [];
-      var countArray = [];
-      var words = this.ticket().description().toLowerCase().replace(/[\.,-\/#!$?%\^&\*;:{}=\-_`~()]/g, "").replace(/\s{2,}/g, " ").split(' ');
-      var title = this.ticket().subject().toLowerCase().replace(/[\.,-\/#!$?%\^&\*;:{}=\-_`~()]/g, "").replace(/\s{2,}/g, " ").split(' ');
-      //words = words.concat(title);
-      var exclusions = this.I18n.t('stopwords.exclusions').split(',');
-      var keywords = _.difference(words, exclusions);
-      title = _.difference(title, exclusions);
-      for (var x = 0; x < keywords.length; x++) {
-        var index = wordsArray.indexOf(keywords[x]);
-        if (index < 0) {
-          keyArray.push({
-            'word': keywords[x],
-            'count': 1
-          });
-          wordsArray.push(keywords[x]);
-          countArray.push(1);
-        } else {
-          keyArray[index].count += 1;
-          countArray[index] += 1;
+        var words = this.ticket().description().toLowerCase().replace(/[\.,-\/#!$?%\^&\*;:{}=\-_`~()]/g,"").replace(/\s{2,}/g," ").split(' ');
+        var title = this.ticket().subject().toLowerCase().replace(/[\.,-\/#!$?%\^&\*;:{}=\-_`~()]/g,"").replace(/\s{2,}/g," ").split(' ');
+        var exclusions = this.I18n.t('stopwords.exclusions').split(',');
+        words = _.difference(words, exclusions);
+        title = _.difference(title, exclusions);
+        
+        var priorityWords = this.countOccurrances(words);
+        var titlePriorityWords = this.countOccurrances(title);
+        var searchTerms = this.getSearchTerms(titlePriorityWords, priorityWords);
+
+        //concates the search terms array into a properly formatted string for the search API
+        var searchTermString = "";
+        for (var x=0; x<searchTerms.length; x++){
+            if(searchTermString != ""){
+                searchTermString += "%20";
+            }
+            searchTermString += searchTerms[x];
         }
-      }
 
-      countArray.sort();
+        return searchTermString;
+    },
 
-      //console.log(countArray);
-
-      //console.log(keyArray);
-      var priorityWords = [];
-      while (countArray.length > 0) {
-        var nextHighest = countArray.pop();
-        for (var j = 0; j < keyArray.length; j++) {
-          if (keyArray[j].count == nextHighest) {
-            priorityWords.push(keyArray.splice(j, 1)[0]);
-            break;
-          }
+    /* 
+     * param: words - array of strings where each string is a word in a set
+     * returns: a sorted array of words from a set
+     */
+    countOccurrances: function(words) {
+        var temp = [];
+        var counts = [];
+        var keys = [];
+        for(var x=0; x<words.length; x++){
+            var index = temp.indexOf(words[x]);
+            if(index < 0){
+                keys.push({'word':words[x], 'count':1});
+                temp.push(words[x]);
+                counts.push(1);
+            }
+            else {
+                keys[index].count += 1;
+                counts[index] += 1;
+            }
         }
-      }
-      wordsArray = [];
-      for (var x = 0; x < title.length; x++) {
-        var index = wordsArray.indexOf(title[x]);
-        if (index < 0) {
-          keyArray.push({
-            'word': title[x],
-            'count': 1
-          });
-          wordsArray.push(title[x]);
-          countArray.push(1);
-        } else {
-          keyArray[index].count += 1;
-          countArray[index] += 1;
+        var keysAndCounts = {'keys': keys, 'counts': counts};
+        return this.sortByCount(keysAndCounts);
+    }, 
+    /* 
+    * returns a sorted array of words by the number of times each word occurrs
+    * Param: kAC => keys and counts object returned from countOccurrances()
+    * Returns: a sorted array of words from a set
+    */
+    sortByCount: function(kAC) {
+        var keys = kAC.keys;
+        var counts = kAC.counts;
+        var tempKeys = [];
+        counts.sort();
+        while (counts.length>0) {
+            var nextHighest = counts.pop();
+            for(var j=0; j<keys.length; j++){
+                if(keys[j].count == nextHighest){
+                    tempKeys.push(keys.splice(j, 1)[0]);
+                    break;
+                }
+            }
         }
-      }
+        return tempKeys;
+    }, 
 
-      var titlePriorityWords = [];
-      while (countArray.length > 0) {
-        var nextHighest = countArray.pop();
-        for (var j = 0; j < keyArray.length; j++) {
-          if (keyArray[j].count == nextHighest) {
-            titlePriorityWords.push(keyArray.splice(j, 1)[0]);
-            break;
-          }
+    /*
+    * param: tPW => title Priority words array
+    * param: pW  => priority words array
+    * returns: array of words to be queried in search
+    */
+    getSearchTerms: function(tPW, pW) {
+        var temp = [];
+        for (var x=0; x<tPW.length; x++){
+            if (x<5) {
+                temp.push(tPW[x].word);
+            }
+            else {
+                break;
+            }
         }
-      }
-
-      var searchTerms = [];
-
-      for (var x = 0; x < titlePriorityWords.length; x++) {
-        if (x < 5) {
-          searchTerms.push(titlePriorityWords[x].word);
-        } else {
-          break;
+        var keysAdded = 0;
+        for(var x=0; x<pW.length; x++){
+            if(keysAdded < 5){
+                if(temp.indexOf(pW[x].word) < 0){
+                    temp.push(pW[x].word);
+                    keysAdded++;
+                }
+            } else {
+                break;
+            }
         }
-      }
-
-      var keysAdded = 0;
-
-      for (var x = 0; x < priorityWords.length; x++) {
-        if (keysAdded <= 5) {
-          if (searchTerms.indexOf(priorityWords[x].word) == -1) {
-            searchTerms.push(priorityWords[x].word);
-            keysAdded++;
-          }
-        } else {
-          break;
-        }
-      }
-
-      var searchTermString = "";
-
-      for (var x = 0; x < searchTerms.length; x++) {
-        if (searchTermString != "") {
-          searchTermString += " ";
-        }
-        searchTermString += searchTerms[x];
-      }
-
-      return searchTermString;
+        return temp;
     }
 
   };
