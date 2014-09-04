440de5184712e4230edf9c836fd2e9acff1ffaa9
Ed Pritchard <epritchard83@gmail.com>
2014-09-04 15:43:22 -0500

    Revert e041453..7a8348e
    This rolls back to commit e041453462fc8020e76db6398163267c616fc40b.

--- app.js
+++ app.js
@@ -1,86 +1,83 @@
 (function() {
 
-  var TFIDF = require('tfidf.js');
+  var Lexer = require('lexer.js');
   return {
     defaultState: 'scaffolding',
 
-    /* Events */
     events: {
       'app.activated': 'doSomething'
     },
-    /* Requests */
+
     requests: {
-      fetchResults: function(searchQuery) {
-        var urlWQuery = "/api/v2/search.json?query=" + searchQuery;
-        return {
-          url: urlWQuery,
-          type: 'GET'
-        };
-      },
-      requestDF: function(term) {
-        return {
-          url: helpers.fmt('/api/v2/search.json?per_page=%@&query=%@ type:topic', 1, term),
-          type: 'GET'
-        };
-      }
-    },
-
-    /* Functions */
-    doSomething: function() {
-      var that = this;
-      var analyzePromises = this.analyzeTicket();
-      this.when(analyzePromises).then(function() {
-        // console.log('extracted Subject terms: ', that.subjectTerms);
-        // console.log('extracted Description terms: ', that.descriptionTerms);
-        var searchQuery = that.subjectTerms.join(" ") + " " + that.descriptionTerms.join(" ");
-        console.log("Search query V2::", searchQuery);
-      });
-
-      var searchwords = this.getKeywords();
-      console.log('Search query V1::', searchwords);
-      this.ajax('fetchResults', searchwords).done(function(data) {
-        for (var resInd = 0; resInd < 5; resInd++) {
-          if (resInd < data.count) {
-            this.$('.results').children(":eq(" + resInd + ")").find('a').text(data.results[resInd].subject);
-            this.$('.results').children(":eq(" + resInd + ")").find('a').attr('href', '/agent/#/tickets/' + data.results[resInd].id);
-          } else {
-            this.$('.results').children(":eq(" + resInd + ")").remove();
-          }
+        fetchResults: function(searchQuery){
+            var urlWQuery = "/api/v2/search.json?query=" + searchQuery;
+            return {
+                url: urlWQuery,
+                type: 'GET'
+            };
         }
-      });
     },
 
-    analyzeTicket: function() {
-      // Get words array from subject and description
-      // Calculate the feature values for terms in both the subject and description
-      var descriptionPromise = TFIDF.analyzeDescription(5, this);
-      var subjectPromise = TFIDF.analyzeSubject(5, this);
+    doSomething: function() {
+      this.extractKeywords();
+      var searchwords = this.getKeywords();
+      this.ajax('fetchResults', searchwords).done(function(data) {
+            for(var resInd = 0; resInd<5; resInd++){
+                if(resInd < data.count){
+                    this.$('.results').children(":eq(" + resInd + ")").find('a').text(data.results[resInd].subject);
+                    this.$('.results').children(":eq(" + resInd + ")").find('a').attr('href', '/agent/#/tickets/' + data.results[resInd].id);
+                } else {
+                    this.$('.results').children(":eq(" + resInd + ")").remove();
+                }
+            }
+        });
+    },
 
-      // todo: (4) Implement checking for intersection of terms and weight higher
-      return this.when(descriptionPromise, subjectPromise);
+    extractKeywords: function() {
+      
+      var description = this.ticket().description();
+      var title = this.ticket().subject();
+
+      //todo: add to the list of excluding common words in zendesk domain (e.g. question,help) 
+      var exclusions = this.I18n.t('stopwords.exclusions').split(',');
+      
+      console.log("pseudoTitle = ");
+      var pseudoTitle = Lexer.pseudoPhrase(title, exclusions);
+      console.log(pseudoTitle);
+
+      console.log("pseudoDescription = ");
+      var pseudoDescription = Lexer.pseudoPhrase(description, exclusions);
+      console.log(pseudoDescription);
+      
+      // combine the title and description to get a list of unique keywords
+      console.log("union of keywords from subject and description: ");
+      var unionKeywords = _.union(pseudoTitle,pseudoDescription);
+      console.log(unionKeywords);
+
+
     },
 
     getKeywords: function() {
-      var words = this.ticket().description().toLowerCase().replace(/[\.,-\/#!$?%\^&\*;:{}=\-_`~()]/g, "").replace(/\s{2,}/g, " ").split(' ');
-      var title = this.ticket().subject().toLowerCase().replace(/[\.,-\/#!$?%\^&\*;:{}=\-_`~()]/g, "").replace(/\s{2,}/g, " ").split(' ');
-      var exclusions = this.I18n.t('stopwords.exclusions').split(',');
-      words = _.difference(words, exclusions);
-      title = _.difference(title, exclusions);
+        var words = this.ticket().description().toLowerCase().replace(/[\.,-\/#!$?%\^&\*;:{}=\-_`~()]/g,"").replace(/\s{2,}/g," ").split(' ');
+        var title = this.ticket().subject().toLowerCase().replace(/[\.,-\/#!$?%\^&\*;:{}=\-_`~()]/g,"").replace(/\s{2,}/g," ").split(' ');
+        var exclusions = this.I18n.t('stopwords.exclusions').split(',');
+        words = _.difference(words, exclusions);
+        title = _.difference(title, exclusions);
+        
+        var priorityWords = this.countOccurrances(words);
+        var titlePriorityWords = this.countOccurrances(title);
+        var searchTerms = this.getSearchTerms(titlePriorityWords, priorityWords);
 
-      var priorityWords = this.countOccurrances(words);
-      var titlePriorityWords = this.countOccurrances(title);
-      var searchTerms = this.getSearchTerms(titlePriorityWords, priorityWords);
-
-      //concates the search terms array into a properly formatted string for the search API
-      var searchTermString = "";
-      for (var x = 0; x < searchTerms.length; x++) {
-        if (searchTermString != "") {
-          searchTermString += "%20";
+        //concates the search terms array into a properly formatted string for the search API
+        var searchTermString = "";
+        for (var x=0; x<searchTerms.length; x++){
+            if(searchTermString != ""){
+                searchTermString += "%20";
+            }
+            searchTermString += searchTerms[x];
         }
-        searchTermString += searchTerms[x];
-      }
 
-      return searchTermString;
+        return searchTermString;
     },
 
     /* 
@@ -88,77 +85,73 @@
      * returns: a sorted array of words from a set
      */
     countOccurrances: function(words) {
-      var temp = [];
-      var counts = [];
-      var keys = [];
-      for (var x = 0; x < words.length; x++) {
-        var index = temp.indexOf(words[x]);
-        if (index < 0) {
-          keys.push({
-            'word': words[x],
-            'count': 1
-          });
-          temp.push(words[x]);
-          counts.push(1);
-        } else {
-          keys[index].count += 1;
-          counts[index] += 1;
+        var temp = [];
+        var counts = [];
+        var keys = [];
+        for(var x=0; x<words.length; x++){
+            var index = temp.indexOf(words[x]);
+            if(index < 0){
+                keys.push({'word':words[x], 'count':1});
+                temp.push(words[x]);
+                counts.push(1);
+            }
+            else {
+                keys[index].count += 1;
+                counts[index] += 1;
+            }
         }
-      }
-      var keysAndCounts = {
-        'keys': keys,
-        'counts': counts
-      };
-      return this.sortByCount(keysAndCounts);
-    },
+        var keysAndCounts = {'keys': keys, 'counts': counts};
+        return this.sortByCount(keysAndCounts);
+    }, 
     /* 
-     * returns a sorted array of words by the number of times each word occurrs
-     * Param: kAC => keys and counts object returned from countOccurrances()
-     * Returns: a sorted array of words from a set
-     */
+    * returns a sorted array of words by the number of times each word occurrs
+    * Param: kAC => keys and counts object returned from countOccurrances()
+    * Returns: a sorted array of words from a set
+    */
     sortByCount: function(kAC) {
-      var keys = kAC.keys;
-      var counts = kAC.counts;
-      var tempKeys = [];
-      counts.sort();
-      while (counts.length > 0) {
-        var nextHighest = counts.pop();
-        for (var j = 0; j < keys.length; j++) {
-          if (keys[j].count == nextHighest) {
-            tempKeys.push(keys.splice(j, 1)[0]);
-            break;
-          }
+        var keys = kAC.keys;
+        var counts = kAC.counts;
+        var tempKeys = [];
+        counts.sort();
+        while (counts.length>0) {
+            var nextHighest = counts.pop();
+            for(var j=0; j<keys.length; j++){
+                if(keys[j].count == nextHighest){
+                    tempKeys.push(keys.splice(j, 1)[0]);
+                    break;
+                }
+            }
         }
-      }
-      return tempKeys;
-    },
+        return tempKeys;
+    }, 
 
     /*
-     * param: tPW => title Priority words array
-     * param: pW  => priority words array
-     * returns: array of words to be queried in search
-     */
+    * param: tPW => title Priority words array
+    * param: pW  => priority words array
+    * returns: array of words to be queried in search
+    */
     getSearchTerms: function(tPW, pW) {
-      var temp = [];
-      for (var x = 0; x < tPW.length; x++) {
-        if (x < 5) {
-          temp.push(tPW[x].word);
-        } else {
-          break;
+        var temp = [];
+        for (var x=0; x<tPW.length; x++){
+            if (x<5) {
+                temp.push(tPW[x].word);
+            }
+            else {
+                break;
+            }
         }
-      }
-      var keysAdded = 0;
-      for (var x = 0; x < pW.length; x++) {
-        if (keysAdded < 5) {
-          if (temp.indexOf(pW[x].word) < 0) {
-            temp.push(pW[x].word);
-            keysAdded++;
-          }
-        } else {
-          break;
+        var keysAdded = 0;
+        for(var x=0; x<pW.length; x++){
+            if(keysAdded < 5){
+                if(temp.indexOf(pW[x].word) < 0){
+                    temp.push(pW[x].word);
+                    keysAdded++;
+                }
+            } else {
+                break;
+            }
         }
-      }
-      return temp;
+        return temp;
     }
 
   };
