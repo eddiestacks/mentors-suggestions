6a21378adeec09583a4e473cda2c5ade20d9b2e8
Cherub Kumar <cherub.kumar@gmail.com>
2014-09-20 16:44:16 -0500

    Remove unneeded functions from app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 (function() {
 
-  var TFIDF = require('tfidf.js');
   var Lexer = require('lexer.js');
   var resultList = [];
   var about = "";
@@ -146,117 +145,6 @@
       }
       //this.switchTo('scaffolding', {resultList:this.resultList, aboutFilter:this.about});
       return this.resultList;
-    },
-
-    analyzeTicket: function() {
-      // Get words array from subject and description
-      // Calculate the feature values for terms in both the subject and description
-      var descriptionKeywords = TFIDF.analyzeDescription(3, this);
-      var subjectKeywords = TFIDF.analyzeSubject(5, this);
-
-      return _.union(descriptionKeywords, subjectKeywords).join(" ");
-    },
-
-    getKeywords: function() {
-      var words = this.ticket().description().toLowerCase().replace(/[\.,-\/#!$?%\^&\*;:{}=\-_`~()]/g, "").replace(/[0-9]/g, "").replace(/\s{2,}/g, " ").split(' ');
-      var title = this.ticket().subject().toLowerCase().replace(/[\.,-\/#!$?%\^&\*;:{}=\-_`~()]/g, "").replace(/[0-9]/g, "").replace(/\s{2,}/g, " ").split(' ');
-      var exclusions = this.I18n.t('stopwords.exclusions').split(',');
-      words = _.difference(words, exclusions);
-      title = _.difference(title, exclusions);
-
-      var priorityWords = this.countOccurrances(words);
-      var titlePriorityWords = this.countOccurrances(title);
-      var searchTerms = this.getSearchTerms(titlePriorityWords, priorityWords);
-
-      //concates the search terms array into a properly formatted string for the search API
-      var searchTermString = "";
-      for (var x = 0; x < searchTerms.length; x++) {
-        if (searchTermString != "") {
-          searchTermString += "%20";
-        }
-        searchTermString += searchTerms[x];
-      }
-
-      return searchTermString;
-    },
-
-    /* 
-     * param: words - array of strings where each string is a word in a set
-     * returns: a sorted array of words from a set
-     */
-    countOccurrances: function(words) {
-      var temp = [];
-      var counts = [];
-      var keys = [];
-      for (var x = 0; x < words.length; x++) {
-        var index = temp.indexOf(words[x]);
-        if (index < 0) {
-          keys.push({
-            'word': words[x],
-            'count': 1
-          });
-          temp.push(words[x]);
-          counts.push(1);
-        } else {
-          keys[index].count += 1;
-          counts[index] += 1;
-        }
-      }
-      var keysAndCounts = {
-        'keys': keys,
-        'counts': counts
-      };
-      return this.sortByCount(keysAndCounts);
-    },
-
-    /* 
-     * returns a sorted array of words by the number of times each word occurrs
-     * Param: kAC => keys and counts object returned from countOccurrances()
-     * Returns: a sorted array of words from a set
-     */
-    sortByCount: function(kAC) {
-      var keys = kAC.keys;
-      var counts = kAC.counts;
-      var tempKeys = [];
-      counts.sort();
-      while (counts.length > 0) {
-        var nextHighest = counts.pop();
-        for (var j = 0; j < keys.length; j++) {
-          if (keys[j].count == nextHighest) {
-            tempKeys.push(keys.splice(j, 1)[0]);
-            break;
-          }
-        }
-      }
-      return tempKeys;
-    },
-
-    /*
-     * param: tPW => title Priority words array
-     * param: pW  => priority words array
-     * returns: array of words to be queried in search
-     */
-    getSearchTerms: function(tPW, pW) {
-      var temp = [];
-      for (var x = 0; x < tPW.length; x++) {
-        if (x < 5) {
-          temp.push(tPW[x].word);
-        } else {
-          break;
-        }
-      }
-      var keysAdded = 0;
-      for (var x = 0; x < pW.length; x++) {
-        if (keysAdded < 5) {
-          if (temp.indexOf(pW[x].word) < 0) {
-            temp.push(pW[x].word);
-            keysAdded++;
-          }
-        } else {
-          break;
-        }
-      }
-      return temp;
     },
 
     manualSearch: function() {
