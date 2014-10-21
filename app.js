c18b31e4be07cfdb41632d4a25f76de72aafaf64
Ed Pritchard <epritchard83@gmail.com>
2014-10-20 20:39:41 -0500

    Renaming templates for better organization

--- app.js
+++ app.js
@@ -199,7 +199,7 @@
           }
         }
 
-        this.switchTo('ticketResults', {
+        this.switchTo('resultsTickets', {
           resultList: resultList,
           aboutFilter: this.aboutFieldContents
         });
@@ -305,7 +305,7 @@
       if (_.isEmpty(data.entries)) {
         return this.switchTo('no_entries');
       } else {
-        this.switchTo('kbResults', data);
+        this.switchTo('resultsKB', data);
       }
     },
 
